8e969eef98ea90f904bddf26515f8cfd20d2a103
UpiXD <96019685+UpiXD@users.noreply.github.com>
2022-07-16 10:25:42 +0700

    Add files via upload
    Responsi UAS Semester 2 Mata Kuliah Pemrograman

---
+++ Mahasiswa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal (21.11.4420)
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string JenisKelamin { get; set; }
+        public float IPK { get; set; }
+    
+    }
+}
